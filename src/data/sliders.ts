2b451f4a14447a9711622658c705d9831362bdac
Andreyyko <oles.andreyko@gmail.com>
2025-01-28 00:08:39 +0200

    slider-responsive

---
+++ src/data/sliders.ts
@@ -0,0 +1,34 @@
+const Slide1 = '/images/slide1.png'
+
+interface Sliders {
+    id: number;
+    title: string;
+    img: string;
+}
+export const sliders: Sliders[] = [
+    {
+        id: 1,
+        title: "Printify Blog",
+        img: Slide1
+    },
+    {
+        id: 2,
+        title: "Printify Blog",
+        img: Slide1
+    },
+    {
+        id: 3,
+        title: "Printify Blog",
+        img: Slide1
+    },
+    {
+        id: 4,
+        title: "Printify Blog",
+        img: Slide1
+    },
+    {
+        id: 5,
+        title: "Printify Blog",
+        img: Slide1
+    }
+];
